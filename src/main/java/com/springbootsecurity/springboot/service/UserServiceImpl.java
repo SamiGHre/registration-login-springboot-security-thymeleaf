df21a49c4379ee6518ca4ef165ae6d71b72aed4f
Sameer K <sameer.kor1998@gmail.com>
2020-11-26 01:43:36 -0800

    Update UserServiceImpl.java

--- src/main/java/com/springbootsecurity/springboot/service/UserServiceImpl.java
+++ src/main/java/com/springbootsecurity/springboot/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.springbootsecurity.springboot.service;
 
+import com.springbootsecurity.springboot.model.Role;
 import com.springbootsecurity.springboot.model.User;
 import com.springbootsecurity.springboot.repository.UserRepository;
 import com.springbootsecurity.springboot.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -11,11 +14,13 @@ public class UserServiceImpl implements UserService{
     private UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository) {
+        super();
         this.userRepository = userRepository;
     }
 
     @Override
     public User save(UserRegistrationDto registrationDto) {
-        User user = new User();
+        User user = new User(registrationDto.getFirstName(), registrationDto.getLastName(), registrationDto.getEmail(), registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
+        return userRepository.save(user);
     }
 }
