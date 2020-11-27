a7495ed2dea19b7ba106abaffb16d7beda0b29d7
Sameer K <sameer.kor1998@gmail.com>
2020-11-27 00:53:26 -0800

    UserRegistrationController, registration thymeleaf template.

---
+++ src/main/java/com/springbootsecurity/springboot/web/dto/UserRegistrationController.java
@@ -0,0 +1,23 @@
+package com.springbootsecurity.springboot.web.dto;
+
+import com.springbootsecurity.springboot.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class UserRegistrationController {
+
+    private UserService userService;
+
+    public UserRegistrationController(UserService userService) {
+        super();
+        this.userService = userService;
+    }
+
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto userRegistrationDto) {
+        userService.save(userRegistrationDto);
+        return "redirect:/registration?success";
+    }
+}
